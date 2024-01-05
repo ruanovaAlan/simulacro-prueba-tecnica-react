47572f0d52a267ebc47de165f52b47ab02d8f540
Alan Ruanova <ruanovaalan96@gmail.com>
2024-01-05 17:44:37 -0600

    Refactor App component to use custom hook for cat facts

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
-import { useState, useEffect } from "react"
 import { useCatImage } from "./hooks/useCatImage"
-import { getRandomFact } from "./services/facts"
+import { useCatFact } from "./hooks/useCatFact"
 import './App.css'
 
 export const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-
-const useCatFact = () => {
-    const [fact, setFact] = useState()
-
-    const refreshFact = () => {
-        getRandomFact().then(({ newFact }) => setFact(newFact))
-    }
-
-    useEffect(() => {
-        refreshFact()
-    }, [])
-
-    return { fact, refreshFact }
-}
 
 export function App() {
     const { fact, refreshFact } = useCatFact()
@@ -32,7 +17,7 @@ export function App() {
             <h1>App de gatitos</h1>
             <button onClick={handleClick}>Change Cat</button>
             {fact && <p>{fact}</p>}
-            {<img src={imageUrl} alt={`Image extracted using first three words of ${fact} `} />}
+            {<img src={imageUrl} alt={`Image extracted using first three words of ${fact}`} />}
         </main>
     )
 }
